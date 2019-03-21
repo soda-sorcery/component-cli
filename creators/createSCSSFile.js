be80da514b5f60be4a695b1f7cef262f1c818185
Daniel Cruse <lawvious@lawvious.domain>
2019-03-20 19:24:21 -0600

    removed console.logs

--- creators/createSCSSFile.js
+++ creators/createSCSSFile.js
@@ -4,18 +4,12 @@ const CryptoJs = require('crypto-js');
 const createSCSSFile = (component, shouldUseHash) => {
 
     const snakeCaseName = component.replace(/[A-Z]/g, '-$&').replace(/-/, '').toLowerCase();
-
-
-    console.log(`generating component for ${snakeCaseName}`);
-    const randomN = ((+(Math.random()*1000).toFixed(3))* 100);
-    console.log(`randomN: ${snakeCaseName}${snakeCaseName}`);
-    const hashedName = CryptoJs.MD5(`${CryptoJs.MD5(`${snakeCaseName}${randomN}`)}`);
-    console.log(`hashedName: ${hashedName}`);
-
-    const className = shouldUseHash ? `${snakeCaseName}-${hashedName}` : `${snakeCaseName}`
+    const nonce = ((+(Math.random()*1000).toFixed(3))* 100);
+    const hashedName = CryptoJs.MD5(`${CryptoJs.MD5(`${snakeCaseName}${nonce}`)}`);
+    const className = shouldUseHash ? `${snakeCaseName}-${hashedName}` : `${snakeCaseName}`;
 
     const cssClass = `.${className} {\n\n\n}`;
-    console.log(`className: ${snakeCaseName}-${hashedName}`);
+    console.log(`className: ${cssClass}`);
 
     const stream = fs.createWriteStream(`./${component}/index.scss`);
     stream.once('open', (fd) => {
